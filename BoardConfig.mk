e48e1f9fff23d0aa839cfdb9932ab6e300711c93
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-12 12:54:22 +0530

    sm6150-user-9-PKQ1.190302.001-compiler01092239-release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,7 +2,7 @@ DEVICE_PATH := device/vivo/1919
 BOARD_VENDOR := vivo
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-11-01
+VENDOR_SECURITY_PATCH := 2019-12-01
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
